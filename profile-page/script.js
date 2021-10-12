4ca3c11134809b424782a952eac4d212909f2fcf
Tiffany Johnson <89874100+TiffanyNicole919@users.noreply.github.com>
2021-10-11 21:08:13 -0400

    Add files via upload

---
+++ profile-page/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+let remove = document.querySelector(".card-list-item")
+function removeAvatar() {
+	remove.remove()
+}
+
+
+let change = document.querySelector(".card-body")
+function changeName() {
+	changeName.editProfile 
+}
